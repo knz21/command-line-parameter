d4be7ce8c780b6bcb6d938ee869ea9d9e5d7b551
knz21 <cismollre@gmail.com>
2024-03-09 12:54:48 +0900

    rename

--- src/test/kotlin/com/knz21/CommandLineParameterTest.kt
+++ src/test/kotlin/com/knz21/CommandLineParameterTest.kt
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
-class MainTest {
+class CommandLineParameterTest {
 
     @Test
     fun testHelpOption() {
